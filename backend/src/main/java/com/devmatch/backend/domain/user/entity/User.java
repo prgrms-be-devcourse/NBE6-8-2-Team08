9090171c7ce0be8a1824374d1218f2f66fb9b5a4
JIWONKIMS <giwon11292@gmail.com>
2025-07-24 09:54:26 +0900

    [BE] feat: User Entity 생성

--- backend/src/main/java/com/devmatch/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/devmatch/backend/domain/user/entity/User.java
@@ -1,4 +1,19 @@
 package com.devmatch.backend.domain.user.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@NoArgsConstructor
 public class User {
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
 }
