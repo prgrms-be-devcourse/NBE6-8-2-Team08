33fdcfb808dae0b7a67a353452671462789ec49e
JIWONKIMS <107270746+JIWONKIMS@users.noreply.github.com>
2025-07-24 17:33:49 +0900

    [BE] feat, chore : Security 추가, user con 수정 (#36)
    * [BE] chore : register 응답 구조 변경
    
    * [BE] chore : register 응답 구조 변경 + security
    
    * [BE] feat, chore : Security 추가, user con 수정

--- backend/src/main/java/com/devmatch/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/devmatch/backend/domain/user/controller/UserController.java
@@ -4,11 +4,13 @@ import com.devmatch.backend.domain.application.entity.Application;
 import com.devmatch.backend.domain.application.service.ApplicationService;
 import com.devmatch.backend.domain.project.entity.Project;
 import com.devmatch.backend.domain.project.service.ProjectService;
-import com.devmatch.backend.domain.user.entity.User;
+import com.devmatch.backend.domain.user.dto.UserRegisterDto;
 import com.devmatch.backend.domain.user.service.UserService;
+import com.devmatch.backend.global.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,18 +30,19 @@ public class UserController {
   private final ApplicationService applicationService;
 
   @PostMapping("/register")
-  public ResponseEntity<String> register(@Valid @RequestBody String name) {
-    User user = userService.save(name);
-    return ResponseEntity.ok("사용자 등록 성공: " + user.getName() + " (ID: " + user.getId() + ")");
+  public ResponseEntity<ApiResponse<UserRegisterDto>> register(@Valid @RequestBody String name) {
+    UserRegisterDto user = userService.save(name);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>("사용자 등록 성공", user));
   }
 
   @GetMapping("/{id}/projects")
   public List<Project> findProjectsById(@PathVariable long id) {
-    return projectService.findByUserId(id);
+    return projectService.getProjects(id);
   }
 
   @GetMapping("/{id}/applications")
   public List<Application> findApplicationsById(@PathVariable long id) {
-    return applicationService.findByUserId(id);
+    return applicationService.getApplications(id);
   }
 }
