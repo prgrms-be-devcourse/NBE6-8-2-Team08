7632117f4a548998c9090ec1ca0f66bc0f5ffc15
SeokGeunHo <kimc6555@gmail.com>
2025-07-25 14:40:43 +0900

    [BE] fix: 지원서 삭제 로직 수정

--- backend/src/main/java/com/devmatch/backend/domain/application/service/ApplicationService.java
+++ backend/src/main/java/com/devmatch/backend/domain/application/service/ApplicationService.java
@@ -3,6 +3,8 @@ package com.devmatch.backend.domain.application.service;
 import com.devmatch.backend.domain.application.dto.response.ApplicationDetailResponseDto;
 import com.devmatch.backend.domain.application.entity.Application;
 import com.devmatch.backend.domain.application.repository.ApplicationRepository;
+import com.devmatch.backend.domain.user.entity.User;
+import com.devmatch.backend.domain.user.repository.UserRepository;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,22 +15,34 @@ import org.springframework.transaction.annotation.Transactional;
 public class ApplicationService {
 
   private final ApplicationRepository applicationRepository;
+  private final UserRepository userRepository;
 
   // 지원서 상세 조회 로직
   @Transactional(readOnly = true)
   public ApplicationDetailResponseDto getApplicationDetail(Long id) {
-    return new ApplicationDetailResponseDto(getApplication(id));
+    return new ApplicationDetailResponseDto(getApplicationByApplicationId(id));
   }
 
   // 지원서 삭제 로직
   @Transactional
   public void deleteApplication(Long id) {
-    applicationRepository.delete(getApplication(id));
+    Application application = getApplicationByApplicationId(id);
+
+    // orphanRemoval = true 로 인해 컬렉션에서 제거하면 자동으로 DB 에서도 삭제됨.
+    application.getUser().removeApplication(application); // 컬렉션에서 제거
+
+//    applicationRepository.delete(application); // DB 에서 삭제
   }
 
-  // 지원서를 가져오는 함수
-  private Application getApplication(Long id) {
+  // 지원서 ID로 지원서를 가져오는 함수
+  private Application getApplicationByApplicationId(Long id) {
     return applicationRepository.findById(id)
         .orElseThrow(() -> new NoSuchElementException("지원서를 찾을 수 없습니다. ID: " + id));
+  }
+
+  // 사용자 ID로 사용자를 가져오는 함수
+  private User getUserByUserId(Long id) {
+    return userRepository.findById(id)
+        .orElseThrow(() -> new NoSuchElementException("사용자를 찾을 수 없습니다. ID: " + id));
   }
 }
