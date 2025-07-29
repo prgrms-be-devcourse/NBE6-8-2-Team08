0d9716a4e37b5a91e414234d5bd21a90661138f0
JIWONKIMS <giwon11292@gmail.com>
2025-07-29 16:31:17 +0900

    [BE] refactor : /backend/security 삭제, 충돌 해결

--- backend/src/main/java/com/devmatch/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/devmatch/backend/domain/user/controller/UserController.java
@@ -1,21 +1,16 @@
 package com.devmatch.backend.domain.user.controller;
 
-import com.devmatch.backend.domain.application.entity.Application;
+import com.devmatch.backend.domain.application.dto.response.ApplicationDetailResponseDto;
 import com.devmatch.backend.domain.application.service.ApplicationService;
 import com.devmatch.backend.domain.project.dto.ProjectDetailResponse;
 import com.devmatch.backend.domain.project.service.ProjectService;
-import com.devmatch.backend.domain.user.dto.UserRegisterDto;
-import com.devmatch.backend.domain.user.service.UserService;
-import com.devmatch.backend.global.ApiResponse;
-import jakarta.validation.Valid;
+import com.devmatch.backend.global.rq.Rq;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,24 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-  private final UserService userService;
+  private final Rq rq;
   private final ProjectService projectService;
   private final ApplicationService applicationService;
 
-  @PostMapping("/register")
-  public ResponseEntity<ApiResponse<UserRegisterDto>> register(@Valid @RequestBody String name) {
-    UserRegisterDto user = userService.save(name);
-
-    return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>("사용자 등록 성공", user));
-  }
-
   @GetMapping("/{id}/projects")
   public ResponseEntity<List<ProjectDetailResponse>> findProjectsById(@PathVariable Long id) {
+    // User user = rq.getActor();
+    // user.getId(); // 현재 로그인한 사용자의 ID를 가져옴
     return ResponseEntity.status(HttpStatus.OK).body(projectService.getProjectsByUserId(id));
   }
 
   @GetMapping("/{id}/applications")
-  public ResponseEntity<List<Application>> findApplicationsById(@PathVariable Long id) {
+  public ResponseEntity<List<ApplicationDetailResponseDto>> findApplicationsById(
+      @PathVariable Long id) {
     return ResponseEntity.status(HttpStatus.OK)
         .body(applicationService.getApplicationsByUserId(id));
   }
