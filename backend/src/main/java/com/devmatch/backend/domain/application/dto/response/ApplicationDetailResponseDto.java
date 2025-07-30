daacfd9a01dd8de5f43bf324a41e2e9160433e7c
DreamTree <166996451+flffkd12@users.noreply.github.com>
2025-07-30 10:24:18 +0900

    [BE] refactor: User엔티티에서 필요한 정보만 응답

--- backend/src/main/java/com/devmatch/backend/domain/application/dto/response/ApplicationDetailResponseDto.java
+++ backend/src/main/java/com/devmatch/backend/domain/application/dto/response/ApplicationDetailResponseDto.java
@@ -2,12 +2,11 @@ package com.devmatch.backend.domain.application.dto.response;
 
 import com.devmatch.backend.domain.application.entity.Application;
 import com.devmatch.backend.domain.application.enums.ApplicationStatus;
-import com.devmatch.backend.domain.user.entity.User;
 import java.time.LocalDateTime;
 
 public record ApplicationDetailResponseDto(
     Long applicationId,                  // 지원서 ID
-    User user,                // 지원자 정보
+    String nickname,                // 지원자 정보
     ApplicationStatus status, // 지원서 승인 상태
     LocalDateTime appliedAt   // 지원 일시
 ) {
@@ -15,7 +14,7 @@ public record ApplicationDetailResponseDto(
   public ApplicationDetailResponseDto(Application application) {
     this(
         application.getId(),
-        application.getUser(),
+        application.getUser().getNickName(),
         application.getStatus(),
         application.getAppliedAt()
     );
